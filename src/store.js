9a83bf21b88b2afa2f317a67859524386565a10f
Andrey Kotlyar <kotlyar562@gmail.com>
2018-07-06 03:39:09 +0300

    Create store, main reducer and saga

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+import reducer from './reducer';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancer = applyMiddleware(sagaMiddleware, logger);
+
+const store = createStore(reducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;

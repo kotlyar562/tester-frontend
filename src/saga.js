f461c8779b1e41e29beee645c36acd6ee7683f34
Andrey Kotlyar <kotlyar562@gmail.com>
2018-07-12 21:38:20 +0300

    Rename main user saga

--- src/saga.js
+++ src/saga.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
-import { watchLoadUser } from './auth/ducks/sagas';
+import { watchUserSaga } from './auth/ducks/sagas';
 
 export default function* rootSaga() {
-  yield all([watchLoadUser()]);
+  yield all([watchUserSaga()]);
 }

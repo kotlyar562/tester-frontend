be8027d4c2265f09eab2a9d61ec0c81d5ffd8898
Andrey Kotlyar <kotlyar562@gmail.com>
2018-07-22 19:28:56 +0300

    Move logic from render() to componentDidUpdate()

--- src/components/StatusInfo.js
+++ src/components/StatusInfo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { notification, Icon } from 'antd';
-import { statusSelector } from '../auth/ducks';
+import { statusSelector, errorsSelector } from '../auth/ducks';
 
 const style = {
   padding: 10,
@@ -10,80 +10,80 @@ const style = {
 };
 
 class StatusInfo extends Component {
-
   componentDidMount() {
     notification.config({
       placement: 'bottomRight',
-      bottom: 50,
-      duration: 3,
+      bottom: 20,
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { status, errors } = this.props;
+    const error = errors && Object.values(errors).join('\n');
+    console.log('errror:', error);
+    const loadingIcon = <Icon type="loading" />;
+    if (status !== prevProps.status) {
+      if (status === 'auth_request') {
+        this.showNotification('info', 'Авторизация...');
+      } else if (status === 'auth_success') {
+        this.showNotification('success', 'Авторизация выполнена успешно');
+      } else if (status === 'auth_error') {
+        this.showNotification('error', 'Ошибка авторизации', error);
+      } else if (status === 'load_user_request') {
+        this.showNotification('info', 'Загрузка...', '', loadingIcon);
+      } else if (status === 'register_request') {
+        this.showNotification('info', 'Регистрация...', '', loadingIcon);
+      } else if (status === 'register_success') {
+        this.showNotification(
+          'success',
+          'Регистрация выполнена успешно',
+          'Мы отправили вам письмо для подтверждения электронной почты.',
+        );
+      } else if (status === 'register_error') {
+        this.showNotification('error', 'Ошибка при регистрации', error);
+      } else if (status === 'change_user_request') {
+        this.showNotification('info', 'Изменяем данные...', '', loadingIcon);
+      } else if (status === 'change_user_success') {
+        this.showNotification('success', 'Изменения сохранены');
+      } else if (status === 'change_user_error') {
+        this.showNotification('error', 'Ошибка при изменении данных', error);
+      } else if (status === 'change_password_request') {
+        this.showNotification('info', 'Изменяем пароль...', '', loadingIcon);
+      } else if (status === 'change_password_success') {
+        this.showNotification('success', 'Пароль успешно изменен');
+      } else if (status === 'change_password_error') {
+        this.showNotification('error', 'Ошибка при изменении пароля', error);
+      }
+    }
+  }
+
+  showNotification = (type, message, description, icon) => {
+    notification[type]({
+      message,
+      description,
+      icon,
+      style,
     });
   }
 
   render() {
-    const { status } = this.props;
-    return (
-      <div>
-        {status === 'auth_request' && notification.info({
-          message: 'Авторизация...',
-          style,
-        })}
-        {status === 'auth_success' && notification.success({
-          message: 'Авторизация выполнена успешно',
-          style,
-        })}
-        {status === 'auth_error' && notification.error({
-          message: 'Ошибка авторизации',
-          style,
-        })}
-        {status === 'load_user_request' && notification.info({
-          message: 'Синхронизация...',
-          style,
-          icon: <Icon type="loading" />,
-        })}
-        {status === 'register_request' && notification.info({
-          message: 'Регистрация...',
-          style,
-        })}
-        {status === 'register_success' && notification.success({
-          message: 'Регистрация выполнена успешно',
-          description: 'Мы отправили вам письмо для подтверждения электронной почты.',
-          style,
-        })}
-        {status === 'register_error' && notification.error({
-          message: 'Ошибка при регистрации',
-          description: 'Проверьте данные и попробуйте еще раз.',
-          style,
-        })}
-        {status === 'change_user_request' && notification.info({
-          message: 'Изменение данных',
-          style,
-          icon: <Icon type="loading" />,
-        })}
-        {status === 'change_user_success' && notification.success({
-          message: 'Изменения сохранены',
-          style,
-        })}
-        {status === 'change_user_error' && notification.error({
-          message: 'Ошибка при изменении данных',
-          description: 'Проверьте данные и попробуйте еще раз.',
-          style,
-        })}
-
-      </div>
-    );
+    return <div />;
   }
 }
 
 StatusInfo.defaultProps = {
   status: '',
+  errors: '',
 };
 
 StatusInfo.propTypes = {
   status: PropTypes.string,
+  errors: PropTypes.object,
 };
 
 const mapStatetoProps = state => ({
   status: statusSelector(state),
+  errors: errorsSelector(state),
 });
 
 export default connect(mapStatetoProps)(StatusInfo);
